dcdcb30b8b7c009cbc823a41806a3aa229e8fddf
Carlos Navarro <cnavarro@c1r2s4.42madrid.com>
2021-07-22 13:53:09 +0200

    parse argument complete, next step: introduce all numbers in the list

--- srcs/parse.c
+++ srcs/parse.c
@@ -6,16 +6,44 @@
 /*   By: cnavarro <cnavarro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 17:04:52 by cnavarro          #+#    #+#             */
-/*   Updated: 2021/07/19 17:52:47 by cnavarro         ###   ########.fr       */
+/*   Updated: 2021/07/22 13:51:05 by cnavarro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
 
+void args_inception(char *str)
+{
+	char **splitted;
+	int i;
+	int j;
+
+	i = 0;
+	splitted = ft_split(str, ' ');
+	while (splitted[i])
+	{
+		j = 0;
+		while(splitted[i][j])
+		{
+			if (!ft_isdigit(splitted[i][j]) && !(splitted[i][j] = '-'))
+			{
+				ft_free_matrix(splitted);
+				ft_strerror("Digit argument not found", 2);
+			}
+			j++;
+		}
+		i++;
+	}
+	ft_free_matrix(splitted);
+}
+
 void	no_args(int argc)
 {
 	if (argc < 2)
+	{
+		ft_strerror("No arguments", 1);
 		exit(0);
+	}
 }
 
 void	args_invalid(char **argv)
@@ -29,10 +57,10 @@ void	args_invalid(char **argv)
 		j = 0;
 		while(argv[i][j])
 		{
-			if (!ft_isdigit(argv[i][j]))
-			{
-				ft_strerror("No digit argument found", 1);
-			}
+			if (ft_strchr(argv[i], ' '))
+				args_inception(argv[i]);
+			else if (!ft_isdigit(argv[i][j]))
+				ft_strerror("Digit argument not found", 2);
 			j++;
 		}
 		i++;
